fc0dcadaa122f5167ab33f3531d9920342a3d3cd
Ricardo Matos <rjcamatos@gmail.com>
2025-01-10 12:45:52 +0000

    ImageConverter file header comparator

--- images/ImageConverter.py
+++ images/ImageConverter.py
@@ -14,16 +14,25 @@ def pack_color(R,G,B):
     return int( (R&0xF8)<<8 | (G&0xFC)<<3 | (B&0xF8)>>3 ).to_bytes(2,'big')
 
 def conv(in_file,out_file):
-    out_file = open(out_file,'wb')
-    in_file = open(in_file,'rb')
+    print("START CONVERSION FROM",in_file,"TO",out_file)
 
+    
+    in_file = open(in_file,'rb')
+    in_file.seek(54)    
+    out_data = bytearray()
+    while True:
+        in_data = in_file.read(3)
+        if len(in_data) == 0: break
+        out_data += pack_color(in_data[2],in_data[1],in_data[0])
+
+    in_file.seek(0)
     bmp_header = struct.unpack_from('<2sI2s2sI',in_file.read(14))
     bmp_header = struct.pack('<2sI2s2sI',
         bmp_header[0],  #BmDescription
-        bmp_header[1],  #FileSize #THE SISZE OF THE HEADER
+        len(out_data)+66,  #FileSize #THE SISZE OF THE HEADER
         bmp_header[2],  #ApplicationSpecific_1
         bmp_header[3],  #ApplicationSpecific_2
-        bmp_header[4]   #PixelArrayOffset
+        66   #PixelArrayOffset
     )
 
     dib_header = struct.unpack_from('<IIIHHIIIIII',in_file.read(40))
@@ -33,25 +42,47 @@ def conv(in_file,out_file):
         dib_header[2],  #ImageHeight
         dib_header[3],  #ColorPlanes
         16,             #BitsPerPixel
-        dib_header[5],  #BiRGB
-        dib_header[6],  #RawBitmapSize
+        3,  #BiRGB
+        len(out_data)+66,  #RawBitmapSize
         dib_header[7],  #PrintResolutionH
         dib_header[8],  #PrintResolutionV
         dib_header[9],  #NumberOfColorsInPallet
         dib_header[10]  #ImportantColors
     )
-    
-    out_file.write(bytes(bmp_header))
-    out_file.write(dib_header)
-    
-    while True:
-        in_data = in_file.read(3)
-        if len(in_data) == 0: break
-        out_data = pack_color(in_data[2],in_data[1],in_data[0])
-        out_file.write(out_data)
-
-    out_file.close()
     in_file.close()
 
+    biMasks = bytearray() #--> NEED TO SEE HOW TO SET THIS MASK TO BIG USE ENDIAN
+    biMasks += int(63488).to_bytes(4,'little') #Red Mask
+    biMasks += int(2016).to_bytes(4,'little') #Blue Mask
+    biMasks += int(31).to_bytes(4,'little') #Green Mask
+
+    out_file = open(out_file,'wb')
+    out_file.write(bytes(bmp_header))
+    out_file.write(dib_header)
+    out_file.write(biMasks)
+    out_file.write(out_data)
+
+    
+    in_file.close()
+    print('CONVERTION DONE !!!')
+
+def print_header(in_file):
+    print("FILE",in_file,"HEADERS")
+    in_file = open(in_file,'rb')
+
+    bmp_header = struct.unpack_from('<2sI2s2sI',in_file.read(14))
+    dib_header = struct.unpack_from('<IIIHHIIIIII',in_file.read(40))
+    bi_colors = struct.unpack_from('<III',in_file.read(12))
+
+    in_file.close()
+
+    print("BMP HEADER",bmp_header)
+    print("DIB HEADER",dib_header)
+    print("BI COLORS",bi_colors)
+
 conv(IN_FILE,OUT_FILE)
-print('CONVERTION DONE !!!')
+
+#print_header('./images/OK_FILE.bmp')
+#print_header('./images/picture16bits.bmp')
+
+
